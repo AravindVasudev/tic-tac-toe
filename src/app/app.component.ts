fb44fefaa66412b8ec5fbd20ba575ab862bd3727
AravindVasudev <aravindv96@yahoo.com>
2018-10-09 00:55:38 +0530

    Added style outline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  board: number[][];
+  boardIcons: string[] = ["", "X", "O"];
+
+  constructor() {
+    this.board = [[0, 0, 0],
+                  [0, 0, 0],
+                  [0, 0, 0]];
+  }
+
+  move(row: number, col: number) {
+    if (!this.board[row][col]) {
+      this.board[row][col] = Math.random() > 0.5 ? 1 : 2;
+    }
+  }
 }
